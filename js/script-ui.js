f170543ae3da0ce3b417da9e7edc15b6a0cc669a
vanta blanta <vantablanta@gmail.com>
2022-03-13 15:49:19 +0300

    adds the what we do description, and jquery effects to hide and show elements

--- js/script-ui.js
+++ js/script-ui.js
@@ -1,9 +1,30 @@
 $(document).ready(function(){
-    $("#scroll").on('click', function(){
-        $(this)
+    $(".design").on('click', function(){
+        $(this).slideUp()
+        $(".design-description").addClass("description-show").slideDown()
+    })
+    $(".design-description").on('click', function(){
+        $(this).slideUp()
+        $(".design").slideDown() 
     })
 
+    $(".dev").on('click', function(){
+        $(this).slideUp()
+        $(".dev-description").addClass("description-show").slideDown()
+    })
+    $(".dev-description").on('click', function(){
+        $(this).slideUp()
+        $(".dev").slideDown() 
+    })
 
+        $(".product").on('click', function(){
+        $(this).slideUp()
+        $(".product-description").addClass("description-show").slideDown()
+    })
+    $(".product-description").on('click', function(){
+        $(this).slideUp()
+        $(".product").slideDown() 
+    })
 
 
 })
